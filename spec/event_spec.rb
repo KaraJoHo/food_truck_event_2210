16305e0a8982e01dfad820314e3dd84dadf6cebf
KaraJH <karas.jones@yahoo.com>
2022-12-27 15:53:35 -0700

    test: Add food truck and food truck names

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -22,4 +22,24 @@ RSpec.describe Event do
     end
   end
 
+  describe '#add_food_truck and #food_truck_names' do 
+    it 'adds food trucks with items to the list of food trucks and their names' do 
+      food_truck1.stock(item1, 35)    
+      food_truck1.stock(item2, 7)  
+
+      food_truck2.stock(item4, 50)    
+      food_truck2.stock(item3, 25)
+
+      food_truck3.stock(item1, 65)  
+
+      event.add_food_truck(food_truck1)    
+      event.add_food_truck(food_truck2)    
+      event.add_food_truck(food_truck3)
+
+      expect(food_trucks).to eq([food_truck1, food_truck2, food_truck3])
+      expect(food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+
+    end
+  end
+
 end
