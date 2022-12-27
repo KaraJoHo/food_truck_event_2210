3f6f2547d65866aecaf799db414f386cd1c5fb28
KaraJH <karas.jones@yahoo.com>
2022-12-27 15:18:11 -0700

    Do over for practice

--- lib/event.rb
+++ lib/event.rb
@@ -1,27 +1,2 @@
-class Event
-  attr_reader :name, :food_trucks
 
-  def initialize(name)
-    @name = name
-    @food_trucks = []
-  end
-
-  def add_food_truck(food_truck_object)
-    @food_trucks << food_truck_object
-  end
-
-  def food_truck_names
-    @food_trucks.map do |truck|
-      truck.name
-    end
-  end
-
-  def food_trucks_that_sell(item_object)
-    @food_trucks.find_all do |truck|
-      truck.inventory.has_key?(item_object)
-    end
-  end
-
-def overstocked
-end
 end
