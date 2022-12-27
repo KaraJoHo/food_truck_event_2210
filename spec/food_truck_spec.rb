ec89dbbc1aed8cdc0051fe9fb20d27ace0ea3253
KaraJH <karas.jones@yahoo.com>
2022-12-27 15:31:55 -0700

    test: Food truck class with attributes

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,1 +1,15 @@
+require './lib/item'
+require './lib/food_truck'
 
+RSpec.describe FoodTruck do 
+  let(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
+  let(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+
+  describe '#initialize' do 
+    it 'exists and has a name and inventory' do 
+      expect(food_truck.name).to eq("Rocky Mountain Pies")
+      expect(food_truck.inventory).to eq({})
+    end
+  end
+end
